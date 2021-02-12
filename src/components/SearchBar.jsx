e9c29876b9e64e5958e3522cb81f90bd2916a5ff
Santiago Rincon <santiagorincon2001@gmail.com>
2021-02-12 10:51:29 -0500

    Uso de estados en la searchbar para acceder al input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
 
-export default function SearchBar(props) {
-  // acá va tu código
-  return (
-  <div className={styles.search}>
-    <div className={styles.buscador}>
-      <input className={styles.input} id="txtCiudad" type="text" placeholder="Ingresa una ciudad"/>
-    </div>
-    <div className={styles.btnBuscar}>
-      <button className={styles.btn} onClick={()=>alert("holi")}>Buscar</button>
-    </div>
-  </div>
-  )
-};
+export default class SearchBar extends React.Component{
+  constructor(props){
+    super(props)
+    this.send = props.send;
+    this.state = {input:""}
+
+    if (this.send === undefined) this.send = () => alert("La funcion send no esta definida")
+    console.log("fe")
+  }
+  saveInputValue(value){
+    this.setState({input:value})
+  }
+
+  render(){
+    return (
+      <div className={styles.search}>
+        <div className={styles.buscador}>
+          <input 
+          className={styles.input} 
+          id="txtCiudad" 
+          type="text" 
+          placeholder="Ingresa un automovil"
+          onChange={(e)=>this.saveInputValue(e.target.value)}/>
+        </div>
+        <div className={styles.btnBuscar}>
+          <button className={styles.btn} onClick={()=>this.send((this.state.input))}>Buscar</button>
+        </div>
+      </div>
+      )
+  }
+}
