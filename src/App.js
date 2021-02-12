d0e06eea34fb7024597ff949447a1c27bfaa6a40
Santiago Rincon <santiagorincon2001@gmail.com>
2021-02-12 12:03:31 -0500

    Se hacen comentarios en app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import "./App.css"
 function App() {
   const [ads, setAd] = useState(getAnuncios());
 
+  //funcion que se encarga de agregar los datos a el estado de react
+  //e hipoteticamente tambien se encarga de subir los datos al backend
   function send(titulo, valor){
+    //Se agregan los datos a el estado del componente
     setAd(antiguo => [...antiguo, {price:valor, title:titulo}])
-    //Aqui deberia ir el llamado al backend
+    //Aqui deberia abajo ir el llamado al backend con fetch o algo asi
   }
+  //Simulacion de la obtencion de datos desde un backend
+  //Igualente aqui deberia ir un fetch pidiendole los datos al backend
   function getAnuncios(){
     return [{price:"50000", title:"BMW"},{price:"10000",title:"AMAROK"}]
   }
+  //Se retorna la searchbar y cars, cuando se actualice la search cars tambien se vera actualizado
   return (
     <div className="app">
        <div>
