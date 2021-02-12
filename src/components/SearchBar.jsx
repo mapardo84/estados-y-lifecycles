79073eacbb25314ac2bf52fc48fc9942a706c563
Santiago Rincon <santiagorincon2001@gmail.com>
2021-02-12 11:58:05 -0500

    Se migra SearchBar a hooks

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './SearchBar.module.css';
 
-export default class SearchBar extends React.Component{
-  constructor(props){
-    super(props)
-    this.send = props.send;
-    this.state = {input:""}
 
-    if (this.send === undefined) this.send = () => alert("La funcion send no esta definida")
-  }
-  saveInputValue(value){
-    this.setState({input:value})
-  }
+export default function SearchBar(props){
+  //Hook para guardar el stado del input
+  const [inputValue, setInputValue] = useState("")
 
-  render(){
-    return (
-      <div className={styles.search}>
-        <div className={styles.buscador}>
-          <input 
-          className={styles.input} 
-          id="txtCiudad" 
-          type="text" 
-          placeholder="Ingresa un automovil"
-          onChange={(e)=>this.saveInputValue(e.target.value)}/>
-        </div>
-        <div className={styles.btnBuscar}>
-          <button className={styles.btn} onClick={()=>this.send((this.state.input),"500")}>Buscar</button>
-        </div>
+  let send = props.send;
+  if (send === undefined) send = () => alert("La funcion send no esta definida")
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.buscador}>
+        <input 
+        className={styles.input} 
+        id="txtCiudad" 
+        type="text" 
+        placeholder="Ingresa un automovil"
+        onChange={(e)=>saveInputValue(e.target.value)}/>
       </div>
-      )
+      <div className={styles.btnBuscar}>
+        <button className={styles.btn} onClick={()=>send((inputValue),"500")}>Buscar</button>
+      </div>
+    </div>
+    )
+
+  function saveInputValue(value){
+    setInputValue(value)
   }
 }
